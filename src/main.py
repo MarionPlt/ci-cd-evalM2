02e92928a0d13ec0403eb8a44c72b09ad4539113
Sairkko <bastien.98@orange.fr>
2025-02-03 19:49:25 +0100

    first commit

---
+++ src/main.py
@@ -0,0 +1,12 @@
+def guess_the_word(user_guess, secret_word="python"):
+    return user_guess.strip().lower() == secret_word.lower()
+
+
+# test
+if __name__ == "__main__":
+    print("Bienvenue dans le jeu Guess the Word!")
+    user_guess = input("Devinez le mot secret : ")
+    if guess_the_word(user_guess):
+        print("Bravo ! Vous avez trouvé le mot secret.")
+    else:
+        print("Dommage, essayez encore !")
